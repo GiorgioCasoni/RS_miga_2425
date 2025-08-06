bee5f9b98df08199da183f6b93bc0884378c1005
GioCasoni <g.casoni2@campus.unimib.it>
2025-08-06 12:00:03 +0200

    Revert "altre modifiche al workspace"
    This reverts commit d13cbbc06310153a131fd7c7f075bc6ff3deffc4.

--- DSenv/pyuic6.bat
+++
@@ -1,1 +0,0 @@
-@C:/Users/gcaso/Documents/progetti_uni/RS-MIGA/DSenv\python.exe -m PyQt6.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
--- src/imports.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sb
-import matplotlib as mp
-import sklearn as skl
-import surprise as sks
-import nltk
-import datasets as ds
