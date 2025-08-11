09daa49c53bba2be5691f1eaef8ba5427131c6bb
Giorgio Casoni <giorgio.casoni.03@gmail.com>
2025-08-11 10:21:22 +0200

    Delete src directory

--- src/base.py
+++
@@ -1,8 +0,0 @@
-from imports import *
-
-dataset_path = 'C:/Users/gcaso/Documents/progetti_uni/RS-MIGA/data/'
-user_reviews = pd.read_parquet(f'{dataset_path}Musical_Instruments.parquet')
-print(user_reviews.head(20))
-print(user_reviews.shape[0])
-print(user_reviews.shape[1])
-
